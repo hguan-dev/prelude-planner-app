17b64f1ae39727c5eae9f13941cd252e6178de98
Harry Guan <guanhuai2014@gmail.com>
2024-07-21 18:21:59 -0500

    cyrus pookie cookie

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 const ProfileScreen = () => {
     return (
         <View>
-            <Text>Profile Screen</Text>
+            <Text>Profile Screen, cyrus is a smart cookie</Text>
         </View>
     );
 };
