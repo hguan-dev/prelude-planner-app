27593b2e5228e015f65452ea4fcd9af1748958e8
Harry Guan <guanhuai2014@gmail.com>
2024-07-24 00:05:37 -0500

    Create NotificationScreen.js
    created simple notificationscreen

---
+++ screens/NotificationScreen.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const NotificationScreen = () => {
+    return (
+        <View>
+            <Text>Notification Screen</Text>
+        </View>
+    );
+};
+
+export default NotificationScreen
