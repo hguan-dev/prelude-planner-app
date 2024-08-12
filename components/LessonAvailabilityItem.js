298a809d51f8ffbd24373900f40373276737b595
grasscodes <gshum0615@gmail.com>
2024-08-12 13:32:40 -0400

    updated lesson availability UI

--- components/LessonAvailabilityItem.js
+++ components/LessonAvailabilityItem.js
@@ -3,7 +3,7 @@ import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
 import { Color, FontFamily, FontSize, Border, Padding } from "../GlobalStyles";
 
 // the component takes in the lesson title and a confirmation status (confirm now or edit time)
-const LessonAvailabilityItem = ({ title, confirmation }) => {
+const LessonAvailabilityItem = ({ title, dates, confirmation }) => {
   // console log for pressing the button, update to link to availability page
   const handlePress = () => {
     console.log("Confirmation button pressed");
@@ -12,10 +12,13 @@ const LessonAvailabilityItem = ({ title, confirmation }) => {
   return (
     <View style={styles.container}>
         <View style={styles.content}>
+          <View style={styles.leftContent}>
             <Text style={styles.title}>{title}</Text>
-            <TouchableOpacity style={styles.button} onPress={handlePress}>
-                <Text style={styles.buttonText}>{confirmation}</Text>
-            </TouchableOpacity>
+            <Text style={styles.subdate}>{dates}</Text>
+          </View>
+          <TouchableOpacity style={styles.button} onPress={handlePress}>
+              <Text style={styles.buttonText}>{confirmation}</Text>
+          </TouchableOpacity>
         </View>
         {/* line for dividing each lesson item */}
         <View style={styles.lessonLine}></View>
@@ -25,47 +28,52 @@ const LessonAvailabilityItem = ({ title, confirmation }) => {
 
 const styles = StyleSheet.create({
     container: {
-      width: '95%',
-      shadowColor: "rgba(0, 0, 0, 0.1)",
-      shadowOffset: { width: 0, height: 2 },
-      shadowOpacity: 0.8,
-      shadowRadius: 4,
-      elevation: 2,
-      marginTop: 8,
-      marginBottom: Padding.p_base,
+      width: "100%",
+      paddingHorizontal: Padding.larger,
+      marginBottom: Padding.default,
     },
     content: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        paddingHorizontal: Padding.p_5xs,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      paddingVertical: Padding.default,
+    },
+    leftContent: {
+      flex: 1,
+      justifyContent: "flex-start",
     },
     title: {
       fontFamily: FontFamily.alataRegular,
-      fontSize: FontSize.defaultBoldHeadline_size,
+      fontSize: FontSize.medium,
       lineHeight: 22,
-      color: Color.labelColorDarkPrimary,
+      color: Color.white,
+      flex: 1,
+      justifyContent: "flex-start",
+    },
+    subdate: {
+      fontFamily: FontFamily.alataRegular,
+      marginTop: 8,
+      lineHeight: 13,
+      fontSize: FontSize.small,
+      color: Color.opacityGray,
       textAlign: "left",
     },
     button: {
       paddingHorizontal: 20,
       paddingVertical: 8,
       borderRadius: 20,
-      backgroundColor: "#B5A3C2",
+      backgroundColor: "#a994b8",
       alignItems: 'center',
       justifyContent: 'center',
     },
     buttonText: {
       fontFamily: FontFamily.alataRegular,
-      fontSize: FontSize.defaultBoldHeadline_size,
+      fontSize: FontSize.medium,
       color: "black",
     },
     lessonLine: {
       height: 1,
-      marginHorizontal: Padding.p_5xs,
-      marginTop: 15,
-      borderBottomLeftRadius: Border.br_sm,
-      borderBottomRightRadius: Border.br_sm,
+      marginTop: 10,
       backgroundColor: "white",
     },
   });
