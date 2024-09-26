1355329f2eab2278f1ed7395674a75a0ddcbf8a5
Eric Chang <55172722+echang594@users.noreply.github.com>
2024-09-25 23:25:59 -0700

    Formatted sql

--- server/index.js
+++ server/index.js
@@ -14,9 +14,19 @@ app.get("/", (req, res) => {
 
 app.get("/event", async (req, res) => {
   try {
-    const result = await pool.query(
-      "SELECT events.id, events.title, events.event_location AS location, users.name AS creator, events.event_description AS description, events.confirmation, events.event_type AS type, events.start_time, events.end_time FROM events INNER JOIN users ON events.creator_id = users.id"
-    );
+    const result = await pool.query(`
+      SELECT events.id,
+             events.title,
+             events.event_location AS location,
+             users.name AS creator,
+             events.event_description AS description,
+             events.confirmation,
+             events.event_type AS type,
+             events.start_time,
+             events.end_time
+      FROM events
+      INNER JOIN users
+      ON events.creator_id = users.id`);
     res.json(result.rows);
   } catch (e) {
     console.log(e);
@@ -36,7 +46,30 @@ app.post("/event/:id", async (req, res) => {
   ];
   try {
     const result = await pool.query(
-      "WITH event AS (UPDATE events SET (title, start_time, end_time, event_location, event_description, event_type) = ($2, $3, $4, $5, $6, $7) WHERE id = $1 RETURNING *) SELECT event.id, event.title, event.event_location AS location, users.name AS creator, event.event_description AS description, event.confirmation, event.event_type AS type, event.start_time, event.end_time FROM event INNER JOIN users ON event.creator_id = users.id",
+      `
+      WITH event AS (
+          UPDATE events SET (
+              title,
+              start_time,
+              end_time,
+              event_location,
+              event_description,
+              event_type
+          ) = ($2, $3, $4, $5, $6, $7)
+          WHERE id = $1
+          RETURNING *
+      ) SELECT event.id,
+               event.title,
+               event.event_location AS location,
+               users.name AS creator,
+               event.event_description AS description,
+               event.confirmation,
+               event.event_type AS type,
+               event.start_time,
+               event.end_time
+      FROM event
+      INNER JOIN users
+      ON event.creator_id = users.id`,
       values
     );
     res.json(result.rows[0]);
@@ -49,7 +82,7 @@ app.post("/event/:id", async (req, res) => {
 // didn't test
 app.delete("/event/:id", async (req, res) => {
   try {
-    await pool.query("DELETE FROM events WHERE id = $1", [req.params.id]);
+    await pool.query(`DELETE FROM events WHERE id = $1`, [req.params.id]);
     res.status(200);
   } catch (e) {
     console.log(e);
@@ -70,7 +103,30 @@ app.post("/event", async (req, res) => {
   ];
   try {
     const result = await pool.query(
-      "WITH event AS (INSERT INTO events (title, creator_id, event_location, event_description, start_time, end_time, event_type) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *) SELECT event.id, event.title, event.event_location AS location, users.name AS creator, event.event_description AS description, event.confirmation, event.event_type AS type, event.start_time, event.end_time FROM event INNER JOIN users ON event.creator_id = users.id",
+      `
+      WITH event AS (
+          INSERT INTO events (
+              title,
+              creator_id,
+              event_location,
+              event_description,
+              start_time,
+              end_time,
+              event_type
+          ) VALUES ($1, $2, $3, $4, $5, $6, $7)
+          RETURNING *
+      ) SELECT event.id,
+               event.title,
+               event.event_location AS location,
+               users.name AS creator,
+               event.event_description AS description,
+               event.confirmation,
+               event.event_type AS type,
+               event.start_time,
+               event.end_time
+      FROM event
+      INNER JOIN users
+      ON event.creator_id = users.id`,
       values
     );
     res.json(result.rows[0]);
